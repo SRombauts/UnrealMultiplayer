e9e934238f5a007dd07d2c313370b5cc91dee490
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2023-11-15 21:58:01 +0100

    Create our own Player Controller & Blueprint

---
+++ Source/UnrealMultiplayer/MyPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2023 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+
+
+#include "MyPlayerController.h"
+
---
+++ Source/UnrealMultiplayer/MyPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALMULTIPLAYER_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
